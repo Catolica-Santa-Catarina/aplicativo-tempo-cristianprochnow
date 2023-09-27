9d401269f927b80841c5e262ab6a8f731d4e34e5
Cristian Prochnow <cristianprochnow@gmail.com>
2023-09-27 20:23:16 -0300

    feat: add get location functionality

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,14 +11,57 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> getLocation() async {
+    await checkLocationPermission();
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high  
+    );
+
+    log(position.toString());
+  }
+
+  Future<void> checkLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnabled) {
+      // serviço de localização desabilitado. Não será possível continuar
+      return Future.error('O serviço de localização está desabilitado.');
+    }
+
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        // Sem permissão para acessar a localização
+        return Future.error('Sem permissão para acesso à localização');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // permissões negadas para sempre
+      return Future.error(
+        'A permissão para acesso a localização foi negada para sempre. '
+        'Não é possível pedir permissão.'
+      );
+    }
+  }
+
+  void onPressedGetLocation() {
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            // obtém a localização atual
-          },
+          onPressed: onPressedGetLocation,
           child: const Text('Obter Localização'),
         ),
       ),
